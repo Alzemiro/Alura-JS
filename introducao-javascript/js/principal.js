0647493e08c276d4afbcf1dd13111d027ccab64b
Alzemiro Iago <iagomoraest@gmail.com>
2019-12-03 22:06:42 -0300

    'One more time'

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,15 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+    console.log(pacientes[i]);
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdIMC = paciente.querySelector(".info-imc");
 
-var tdIMC = paciente.querySelector(".info-imc")
-var imc = peso / (altura * altura); // 100 / (2 * 2) ---> 100 / 4 ---> 25
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
 
-tdIMC.textContent = imc;
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        tdPeso.textContent = "Peso inválido!";
+        pesoEhValido = false;
+    }
+
+    if (altura <= 0 || altura > 3) {
+        console.log("Altura inválido!");
+        tdAltura.textContent = "Altura inválido!";
+        alturaEhValido = false;
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura); // 100 / (2 * 2) ---> 100 / 4 ---> 25
+        tdIMC.textContent = imc.toFixed(2);
+    } else {
+        tdIMC.textContent = "Altura e/ou peso inválidos";
+    }
+}
