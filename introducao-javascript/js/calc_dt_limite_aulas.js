544eb2abf342fea7d2c40b8c033b3b026bf3fcc2
Alzemiro Iago <iagomoraest@gmail.com>
2019-12-05 20:44:43 -0300

    teste

--- introducao-javascript/js/calc_dt_limite_aulas.js
+++ introducao-javascript/js/calc_dt_limite_aulas.js
@@ -16,7 +16,7 @@ function getWeekdaysInMonth(month, year) {
     return weekdays;
 }
 
-var weekdays = getWeekdaysInMonth(02, 2019);
+var weekdays = getWeekdaysInMonth(11, 2019);
 
 var teste = document.querySelector("#teste");
 
