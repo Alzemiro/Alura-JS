6067710f5090a95365300940c71061a716881b8f
Alzemiro Iago <iagomoraest@gmail.com>
2019-12-05 20:37:42 -0300

    teste

---
+++ introducao-javascript/js/calc_dt_limite_aulas.js
@@ -0,0 +1,23 @@
+function daysInMonth(ano, mes) {
+    return 32 - new Date(ano, mes, 32).getDate();
+}
+
+function isWeekday(year, month, day) {
+    var day = new Date(year, month, day).getDay();
+    return day != 0 && day != 6;
+}
+
+function getWeekdaysInMonth(month, year) {
+    var days = daysInMonth(month, year);
+    var weekdays = 0;
+    for (var i = 0; i < days; i++) {
+        if (isWeekday(year, month, i + 1)) weekdays++;
+    }
+    return weekdays;
+}
+
+var weekdays = getWeekdaysInMonth(01, 2019);
+
+var teste = document.querySelector("#teste");
+
+teste.textContent = weekdays;
